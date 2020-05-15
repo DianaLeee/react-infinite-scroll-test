68b65eafc012d31dfa23b747276e7719a4d1d06b
DianaLeee <yeonylee94@gmail.com>
2020-05-15 17:36:14 +0900

    Fix TopHeader to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React, { useEffect } from "react";
 import Main from "./components/Main";
 import TopHeader from "./components/TopHeader";
+import styled from "styled-components";
 
+const FixedHeaderWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 100;
+`;
 const App = () => {
   useEffect(() => {
     document.title = "Dianastagram";
@@ -9,7 +17,9 @@ const App = () => {
 
   return (
     <div>
-      <TopHeader />
+      <FixedHeaderWrapper>
+        <TopHeader />
+      </FixedHeaderWrapper>
       <Main />
     </div>
   );
